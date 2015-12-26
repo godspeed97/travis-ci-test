82b91d390b70e14bc4871b188f39afc32c5e883d
oliviertr <olivier.tr@hotmail.com>
2015-12-26 01:54:26 -0500

    last try phantomjs

--- my.conf.js
+++ my.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration
-// Generated on Sat Dec 26 2015 01:52:34 GMT-0500 (Eastern Standard Time)
+// Generated on Sat Dec 26 2015 01:54:16 GMT-0500 (Eastern Standard Time)
 
 module.exports = function(config) {
   config.set({
@@ -55,7 +55,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
